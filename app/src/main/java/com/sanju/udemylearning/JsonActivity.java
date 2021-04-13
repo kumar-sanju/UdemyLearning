ad133c8cd8ab0d27521a5144a327c32bd7f1e985
prantae-desktop-005 <mailmeinmymail0@gmail.com>
2021-04-13 16:54:17 +0530

    new layout

---
+++ app/src/main/java/com/sanju/udemylearning/JsonActivity.java
@@ -0,0 +1,31 @@
+package com.sanju.udemylearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class JsonActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json);
+
+        btn = findViewById(R.id.btn);
+        text = findViewById(R.id.text);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
